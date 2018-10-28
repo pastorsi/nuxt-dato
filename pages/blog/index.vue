32c37da15bce82020dc7ae4a93b2e25b702d7be7
Simon Measures <pastorsi@gmail.com>
2018-10-28 23:05:42 +0000

    Second commit

---
+++ pages/blog/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    All blog posts
+    <ul>
+      <li
+      v-for="(post, index) in allPosts"
+      :key="index"
+    >
+      <h2>
+        <nuxt-link :to="`/blog/${post.slug}`">
+          {{ post.title }}
+        </nuxt-link>
+      </h2>
+      <p>{{ post.text }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+
+export default {
+apollo: {
+allPosts: gql`{
+allPosts {
+title
+text
+slug
+}
+}'
+}
+}
+</script>
